a55c2801e6a429fbfa15bd757b92c3169ad9569f
leo-carrey <leo.carrey@laplateforme.io>
2023-05-09 12:59:33 +0200

    [ADD] job05

---
+++ jour01/job05/script.js
@@ -0,0 +1,11 @@
+function afficherjourssemaines(){
+    arr=["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]
+var x = 0
+for (let i = 0; i < 7; i++) {
+    console.log(arr[x]);
+    x+=1;
+}
+
+}
+
+afficherjourssemaines()
